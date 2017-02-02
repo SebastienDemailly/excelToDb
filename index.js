a4cef82aad775e76064a2a7206ee1efa76d856a7
SebastienD <sebastien.demailly@arvato.fr>
2017-02-02 11:53:40 +0100

    correction 2 district orderId parameter

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var excelToJson = require('./excelToJson');
 var jsonToDb = require('./jsonToDb');
 
 module.exports = {
-  excelIntegrationDb: function(excelFilePath, orderId) {
-    return bResult = jsonToDb.insert(excelToJson.convert(excelFilePath, orderId));
-  }
+    excelIntegrationDb: function(excelFilePath, orderId) {
+        return bResult = jsonToDb.insert(excelToJson.convert(excelFilePath), orderId);
+    }
 }
