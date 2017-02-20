04021afb43f5117b1e540b85396dcd54ccec9f01
SebastienD <sebastien.demailly@arvato.fr>
2017-02-20 14:26:01 +0100

    promise correction

--- jsonToDb.js
+++ jsonToDb.js
@@ -5,27 +5,27 @@ var asyncc = require('async');
 module.exports = {
     insert: function(json, orderId) {
         return new Promise(function(resolve, reject) {
-                console.log("--------------------- DEBUT INSERTION BDD ---------------------")
-                initReferencesTypes(json).then(function() {
-                    return initFormats(json)
-                }).then(function() {
-                    return initReferences(json)
-                }).then(function() {
-                    return initDistributors(json)
-                }).then(function() {
-                    return initDistricts(json, orderId)
-                }).then(function() {
-                    return initCities(json)
-                }).then(function() {
-                    return initCityReferences(json)
-                }).then(function() {
-                    console.log("--------------------- FIN INSERTION BDD ---------------------")
-                    return resolve(true);
-                }).catch(function(err) {
-                    reject(err);
-                })
-            }
+            console.log("--------------------- DEBUT INSERTION BDD ---------------------")
+            initReferencesTypes(json).then(function() {
+                return initFormats(json)
+            }).then(function() {
+                return initReferences(json)
+            }).then(function() {
+                return initDistributors(json)
+            }).then(function() {
+                return initDistricts(json, orderId)
+            }).then(function() {
+                return initCities(json)
+            }).then(function() {
+                return initCityReferences(json)
+            }).then(function() {
+                console.log("--------------------- FIN INSERTION BDD ---------------------")
+                return resolve(true);
+            }).catch(function(err) {
+                reject(err);
+            })
         })
+    }
 }
 
 var initReferencesTypes = function(json) {
